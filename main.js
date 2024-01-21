029bc0e4cbcc5ea096aed76d80bdb27a9a4812b1
MarkMoorefield <155267237+MarkMoorefield@users.noreply.github.com>
2024-01-21 03:52:01 +0000

    Added the logic to the solution.

--- main.js
+++ main.js
@@ -1,3 +1,8 @@
+// Created a variable called binaryArrayToNumber (the goal of the problem).
+// made it an arrow function with the perameter of arr.
+// then I made the function return parseInt (which turns a string into a integer).
+// then I made it join the array together. 
+
 const binaryArrayToNumber = arr => {
     return parseInt(arr.join(""), 2)
 };
